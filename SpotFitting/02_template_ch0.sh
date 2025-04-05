51270c97202fe67ed0512e86a467e0bcf2067963
irelamb <61142146+irelamb@users.noreply.github.com>
2025-04-05 12:47:39 +0200

    Update 02_template_ch0.sh

--- SpotFitting/02_template_ch0.sh
+++ SpotFitting/02_template_ch0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 #SBATCH --job-name=JOB_NAME
-#SBATCH --output=/scratch/ilambert/SunTag/JOB_NAME_%A_%a.out
-#SBATCH --error=/scratch/ilambert/SunTag/JOB_NAME_%A_%a.err
+#SBATCH --output=JOB_NAME_%A_%a.out
+#SBATCH --error=JOB_NAME_%A_%a.err
 #SBATCH --time=2:00:00
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
@@ -20,9 +20,6 @@ dt=20 # seconds
 
 model_name="MODEL_NAME"
 
-#chan0_avg=CHAN0_AVG # average intensity green channel (laser power 100 -> ~500)
-#chan0_std=55 # (laser power 100 -> ~100?)
-
 path="PATH_IN"
 file_names=($(ls $path)) # file names of input images (either .tif or .ome.tif)
 
@@ -31,4 +28,3 @@ file_name=${file_names[${SLURM_ARRAY_TASK_ID}]}
 source activate "PATH_ENV"
 
 python3 main/makeFit_distributed.py $file_name "0" $model_name $dx $dy $dt # channel 0
-#python3 main/makeFit_distributed_4calibration.py $file_name "0" $chan0_avg $chan0_std $model_name $dx $dy $dt # channel 0
