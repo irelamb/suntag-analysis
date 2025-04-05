1437f5070f4d8a63039a1573e062d0e5ad015584
irelamb <61142146+irelamb@users.noreply.github.com>
2025-04-05 12:45:28 +0200

    Update 01_submit_job.sh

--- SpotFitting/01_submit_job.sh
+++ SpotFitting/01_submit_job.sh
@@ -2,6 +2,7 @@
 
 # to be modified by the user #
 input_path="..." # path to input data (tif/ometif)
+env_path="..." # path to python environment
 job_name="test"
 #
 
@@ -11,12 +12,14 @@ echo $n_files
 
 cp 02_template_ch0.sh 03_slurm_ch0.sh
 sed -i "s#PATH_IN#$input_path#" 03_slurm_ch0.sh
+sed -i "s#PATH_ENV#$env_path#" 03_slurm_ch0.sh
 sed -i "s/N_FILES/$(($n_files-1))/" 03_slurm_ch0.sh
 sed -i "s#JOB_NAME#$job_name#" 03_slurm_ch0.sh
 sed -i "s#MODEL_NAME#$model_name#" 03_slurm_ch0.sh
 
 cp 02_template_ch1.sh 03_slurm_ch1.sh
 sed -i "s#PATH_IN#$input_path#" 03_slurm_ch1.sh
+sed -i "s#PATH_ENV#$env_path#" 03_slurm_ch1.sh
 sed -i "s/N_FILES/$(($n_files-1))/" 03_slurm_ch1.sh
 sed -i "s#JOB_NAME#$job_name#" 03_slurm_ch1.sh
 sed -i "s#MODEL_NAME#$model_name#" 03_slurm_ch1.sh
